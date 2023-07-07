6ea67f9c90dd7d65e05379e35498514910edb897
André Moraes <andre.moraes@dtidigital.com.br>
2023-07-07 15:41:19 -0300

    feat: installing package to format date
    work item: 0003

--- src/components/Comments.js
+++ src/components/Comments.js
@@ -1,16 +1,18 @@
 import React from "react";
+import { formatRelative } from "date-fns";
+import { ptBR } from "date-fns/locale";
+
 import './Comments.css';
 
-const Comments = (props) => {
-    const formattedDate = props.date.toLocaleDateString();
 
+const Comments = (props) => {
     return (
     <div className="Comments">              
         <div className="card">
             <h2 className="name">{props.name} </h2>
             <p className="email">{props.email}</p>
             <p className="mensage">{props.children}</p>           
-            <p className="date">{formattedDate}</p>        
+            <p className="date">{formatRelative(props.date, new Date(), {locale: ptBR})}</p>        
             <button onClick={props.onRemove}>&times;</button>
         </div>
     </div>    
