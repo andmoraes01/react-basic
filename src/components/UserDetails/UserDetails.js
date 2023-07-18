4ca002859c0bd860a1816ab802e8357efaef1cdb
André Moraes <andre.moraes@dtidigital.com.br>
2023-07-17 23:50:17 -0300

    feat: indent adjustments
    work item: 0000

--- src/components/UserDetails/UserDetails.js
+++ src/components/UserDetails/UserDetails.js
@@ -24,10 +24,10 @@ function UserDetails() {
             })
     }, [userId])
 
-    if(user.name !== undefined) {
+    if (user.name !== undefined) {
         return <>
             <h1>{user.name} {user.lastName}</h1>
-            <img src={user.photo} alt={user.name}/>
+            <img src={user.photo} alt={user.name} />
             <p>{user.email}</p>
             <Link to="/usuarios"> Votlar</Link>
         </>
